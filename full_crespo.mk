ed9a6bff4a38e8dd10a927d672ea7a6ebd0d9140
carz2 <carz8500@gmail.com>
2011-12-09 16:58:51 -0700

    No recovery in ota package, add some common cm stuff, add lunch combo

--- full_crespo.mk
+++ full_crespo.mk
@@ -25,9 +25,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/crespo/device.mk)
 
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_crespo
 PRODUCT_DEVICE := crespo
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus S
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := NS
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=GWK74 BUILD_FINGERPRINT=google/soju/crespo:2.3.7/GWK74/185293:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.7 GWK74 185293 release-keys" BUILD_NUMBER=185293
