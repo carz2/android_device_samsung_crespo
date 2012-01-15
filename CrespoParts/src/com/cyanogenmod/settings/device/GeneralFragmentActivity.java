9c8b901d2ba17c5399395f2ef31a8bebf218158b
carz2 <carz8500@gmail.com>
2012-01-14 21:55:39 -0700

    Revert "crespo parts: remove backlight timer setting, add 'no blink' to blink timer setting"
    This reverts commit 3f4805883d8e124f1d6ef5c8bcc64a9e215ef4a3.

--- CrespoParts/src/com/cyanogenmod/settings/device/GeneralFragmentActivity.java
+++ CrespoParts/src/com/cyanogenmod/settings/device/GeneralFragmentActivity.java
@@ -39,6 +39,7 @@ public class GeneralFragmentActivity extends PreferenceFragment {
 
     private CheckBoxPreference mDeepIdle;
     private CheckBoxPreference mNotification;
+    private ListPreference mBacklightTimeout;
     private ListPreference mBlinkTimeout;
     private ListPreference mBlinkInterval;
 
@@ -63,6 +64,10 @@ public class GeneralFragmentActivity extends PreferenceFragment {
         } else {
             mNotification.setEnabled(false);
         }
+
+        mBacklightTimeout = (ListPreference) findPreference(DeviceSettings.KEY_BACKLIGHT_TIMEOUT);
+        mBacklightTimeout.setEnabled(TouchKeyBacklightTimeout.isSupported());
+        mBacklightTimeout.setOnPreferenceChangeListener(new TouchKeyBacklightTimeout());
 
         mBlinkTimeout = (ListPreference) findPreference(DeviceSettings.KEY_BLINK_TIMEOUT);
         mBlinkTimeout.setEnabled(TouchKeyBlinkTimeout.isSupported());
