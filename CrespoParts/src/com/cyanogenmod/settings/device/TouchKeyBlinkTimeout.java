f0df50d05679a620af72e4d528e66f5ffd765e42
carz2 <carz8500@gmail.com>
2012-07-15 18:37:38 -0600

    add crespo parts back in

---
+++ CrespoParts/src/com/cyanogenmod/settings/device/TouchKeyBlinkTimeout.java
@@ -0,0 +1,36 @@
+package com.cyanogenmod.settings.device;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceManager;
+
+public class TouchKeyBlinkTimeout implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/class/misc/notification/max_blink_count";
+
+    public static boolean isSupported() {
+        return Utils.fileExists(FILE);
+    }
+
+    /**
+     * Restore backlight timeout setting from SharedPreferences. (Write to kernel.)
+     * @param context       The context to read the SharedPreferences from
+     */
+    public static void restore(Context context) {
+        if (!isSupported()) {
+            return;
+        }
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Utils.writeValue(FILE, sharedPrefs.getString(DeviceSettings.KEY_BLINK_TIMEOUT, "600"));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Utils.writeValue(FILE, (String) newValue);
+        return true;
+    }
+
+}
